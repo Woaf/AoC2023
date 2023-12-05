c7a35c55af24aa24d47f53900791ab2f87e1254b
Elyx <balintbezerk3r@gmail.com>
2023-12-05 21:52:41 +0100

    Added raw data file for the Dec 2 task

--- AdventOfCode/02_MarbleGame/02_MarbleGame/main.cpp
+++ AdventOfCode/02_MarbleGame/02_MarbleGame/main.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
 int main()
 {
+	std::ifstream rawDataFile("rawdata.txt");
+	if (!rawDataFile.is_open())
+	{
+		std::cout << "No input file found..." << std::endl;
+		return -1;
+	}
+
 
 
 	return 0;
